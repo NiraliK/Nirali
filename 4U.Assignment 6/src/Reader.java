6a833751deca31309d72867c9791bd9000f20a4f
niralikantaria <niralikantaria@172.21.50.177>
2017-12-07 10:09:30 -0500

    working on the reader

---
+++ 4U.Assignment 6/src/Reader.java
@@ -0,0 +1,33 @@
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author niralikantaria
+ */
+class Reader {
+    /**
+     * reads file and returns image 
+     * @param filename read the scenarios 
+     * @return the image
+     */
+    public SceneInputs readFile(String filename){
+        //file reader is intialized 
+        FileReader file = null;
+        
+        Scanner input = new Scanner(file);
+        String loc = input.nextLine();
+        String dir = input.nextLine();
+        
+           
+        }
+    } 
+    
+
